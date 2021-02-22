a6637f5d27a7f725c7e9dfb1bdfe2abc6b360460
ZEXIN GUO <a1025922331@163.com>
2021-02-22 08:39:05 +0800

    2021-2-17

---
+++ Leetcode/src/Exercise0084_largest_rectangle_in_histogram.java
@@ -0,0 +1,7 @@
+public class Exercise0084_largest_rectangle_in_histogram {
+    public int largestRectangleArea(int[] heights) {
+        return 0;
+    }
+}
+// 1. O(n^3)、O(1): 枚举左右边界，扫描中间的最小值
+// 2. O(n^2)、O(1): 枚举高度、
