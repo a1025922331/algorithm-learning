c198de59dc14c4a802718529ff7bb26aeeda8a73
ZEXIN GUO <a1025922331@163.com>
2021-08-06 00:24:23 +0800

    2021-8-6

--- Leetcode/src/Exercise0039_组合总数.java
+++ Leetcode/src/Exercise0039_组合总数.java
@@ -1,28 +1,25 @@
 import java.util.*;
 
 public class Exercise0039_组合总数 {
-    public static void main(String[] args) {
-        Exercise0039_组合总数 solution = new Exercise0039_组合总数();
-        System.out.println(solution.combinationSum(new int[]{2, 3, 6, 7}, 7));
-    }
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<Integer> list = new ArrayList<>();
-        Set<List<Integer>> result = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
+        // 先排序便于后续剪枝
         Arrays.sort(candidates);
-        dfs(candidates, target, 0, 0, list, result);
-        return new ArrayList<>(result);
+        dfs(candidates, target, 0, list, result);
+        return result;
     }
 
-    private void dfs(int[] candidates, int target, int cur, int sum, List<Integer> list, Set<List<Integer>> result) {
-        if (sum == target) {
+    private void dfs(int[] candidates, int target, int cur, List<Integer> list, List<List<Integer>> result) {
+        if (target == 0) {
             result.add(new ArrayList<>(list));
             return;
         } else {
             for (int i = cur; i < candidates.length; i++) {
-                if (sum + candidates[i] <= target) {
+                // 剪枝
+                if (candidates[i] <= target) {
                     list.add(candidates[i]);
-                    dfs(candidates, target, i, sum + candidates[i], list, result);
+                    dfs(candidates, target - candidates[i], i, list, result);
                     list.remove(list.size() - 1);
                 } else {
                     break;
@@ -31,3 +28,4 @@ public class Exercise0039_组合总数 {
         }
     }
 }
+
